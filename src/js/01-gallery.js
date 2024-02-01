e8f994375c28b5be938beabe81649e8f7e5ebf8a
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-02-02 00:06:57 +0800

    try video

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const lightbox = document.querySelector(".gallery");
 
+// Generate HTML markup for each image
+const images = galleryItems.map(image => `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+      <img
+        class="gallery__image"
+        src="${image.preview}"
+        data-source="${image.original}"
+        alt="${image.description}"
+      />
+    </a>
+  </div>`
+).join("");
+
+// Insert the generated HTML markup into the gallery container
+lightbox.innerHTML = images;
+
+// Initialize SimpleLightbox
+const lightboxInstance = new SimpleLightbox(".gallery a", {});
 console.log(galleryItems);
+
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+// Function to format time in minutes and seconds
+const formatTime = (timeInSeconds) => {
+  const minutes = Math.floor(timeInSeconds / 60);
+  const seconds = Math.floor(timeInSeconds % 60);
+  return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+};
+
+// Function to parse time in minutes and seconds format to seconds
+const parseTimeToSeconds = (timeString) => {
+  const [minutes, seconds] = timeString.split(':').map(parseFloat);
+  return (minutes * 60) + seconds;
+};
+
+// Initialize player
+player.ready().then(() => {
+  // Retrieve playback time from local storage on page load
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    const totalSeconds = parseTimeToSeconds(currentTime);
+    player.setCurrentTime(totalSeconds);
+  }
+
+  // Track playback time update and save to local storage
+  player.on('timeupdate', throttle((data) => {
+    const currentTime = data.seconds;
+    const formattedTime = formatTime(currentTime);
+    console.log('Current playback time:', formattedTime);
+    localStorage.setItem('videoplayer-current-time', formattedTime);
+  }, 1000));
+});
