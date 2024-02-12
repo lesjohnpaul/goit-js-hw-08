ca50972ae881a0e5167f7de032c161bf2eb92efe
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-02-12 23:05:43 +0800

    for submission

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,8 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 const lightbox = document.querySelector(".gallery");
 
-// Generate HTML markup for each image
 const images = galleryItems.map(image => `
   <div class="gallery__item">
     <a class="gallery__link" href="${image.original}">
@@ -18,11 +15,8 @@ const images = galleryItems.map(image => `
     </a>
   </div>`
 ).join("");
-
-// Insert the generated HTML markup into the gallery container
 lightbox.innerHTML = images;
 
-// Initialize SimpleLightbox
 const lightboxInstance = new SimpleLightbox(".gallery a", {});
 console.log(galleryItems);
 
