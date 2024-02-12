73ae05c9141f4b863a17f84ffe346b62ca86d215
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-02-12 23:02:00 +0800

    hw8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const formElement = document.querySelector('.feedback-form');
+const emailInput = formElement.querySelector('input[name="email"]');
+const messageTextarea = formElement.querySelector('textarea[name="message"]');
+
+loadFormData();
+
+formElement.addEventListener('submit', onSubmit);
+formElement.addEventListener('input', throttle(onInput, 500));
+
+function onSubmit(event) {
+  event.preventDefault();
+  if (emailInput.value === '' || messageTextarea.value === '') {
+    return;
+  }
+  const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  console.log('Form submitted with data:', formData);
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onInput(event) {
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function loadFormData() {
+  const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (formData) {
+    if (formData.message) {
+      messageTextarea.value = formData.message;
+    }
+    if (formData.email) {
+      emailInput.value = formData.email;
+    }
+  }
+}
